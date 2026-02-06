c9975046cfa09752b54534661fc301bb0ee1836f
xuyinhan <xuyinhan4541@qq.com>
2026-02-06 16:23:55 +0800

    Initial Commit

---
+++ Earthlord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Earthlord
+//
+//  Created by 许意晗 on 2026/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Earthlord/EarthlordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthlordApp.swift
+//  Earthlord
+//
+//  Created by 许意晗 on 2026/2/6.
+//
+
+import SwiftUI
+
+@main
+struct EarthlordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
